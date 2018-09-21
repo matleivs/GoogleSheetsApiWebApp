7445b2762323203e26523f94b1172be88840a15f
leili <matleivs@gmail.com>
2018-09-21 19:59:51 +0200

    adding a new route to our Application which takes the Request from the spreadsheet controls, Calls the helper which  creates the spreadsheet, and Saves a record in the Database

--- routes.js
+++ routes.js
@@ -88,7 +88,31 @@ router.post('/upsert', function (req, res, next) {
   });
 });
 
-// TODO: Add route for creating spreadsheet.
+// DONE: Add route for creating spreadsheet.
+var SheetsHelper = require('./sheets'); // this refers to the helper class defined in sheets.js File in the project root dir
+
+router.post('/spreadsheets', function (req, res, next) {
+  var auth = req.get('Authorization');
+  if (!auth) {
+    return next(Error('Authorization required!'));
+  }
+  var accessToken = auth.split(' ')[1];
+  var helper = new SheetsHelper(accessToken);
+  var title = 'Orders (' + new Date().toLocaleTimeString() + ')';
+  helper.createSpreadsheet(title, function (err, spreadsheet) {
+    if (err) {
+      return next(err);
+    }
+    var model = {
+      id: spreadsheet.spreadsheetId,
+      sheetId: spreadsheet.sheets[0].properties.sheetId,
+      name: spreadsheet.properties.title
+    };
+    models.Spreadsheet.create(model).then(function () {
+      return res.json(model);
+    });
+  });
+});
 
 
 
