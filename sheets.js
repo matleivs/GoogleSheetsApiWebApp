7b3c283edaf9af5afbc871005729917ef3d89a87
leili <matleivs@gmail.com>
2018-09-21 19:49:55 +0200

    adding a method that defines a simple Spreadsheet object and creates a spreadsheet on the server

--- sheets.js
+++ sheets.js
@@ -14,3 +14,38 @@ var SheetsHelper = function(accessToken) {
     this.service = google.sheets({version: 'v4', auth: auth});
 };
 module.exports = SheetsHelper;
+
+/** A method for Creating a spreadsheet.
+ * This method defines a simple Spreadsheet Object and 
+ * calls the API's spreadsheets.create method to create it on the Server.
+ */
+SheetsHelper.prototype.createSpreadsheet = function(title, callback) {
+    var self = this;
+    var request = {
+        resource: {
+            properties: {
+                title: title
+            },
+            sheets: [
+                {
+                    properties: {
+                        title: 'Data',
+                        gridProperties: {
+                            columnCount: 6,
+                            frozenRowCount: 1
+                        }
+                    }
+                },
+                // TODO: Add more sheets into this array
+            ]
+        }
+    };
+    self.service.spreadsheets.create(request, function(err, response) {
+        if(err) {
+            return callback(err);
+        }
+        var spreadsheet = response.data;
+        // TODO: Add header Rows
+        return callback(null, spreadsheet);
+    });
+};
